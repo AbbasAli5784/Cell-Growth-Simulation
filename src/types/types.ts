ad59d07b497cba10e886abd751b72d593276c8f5
Abbas Ali <abbasali@Abbass-MacBook-Pro.local>
2025-06-16 15:16:45 -0400

    Define CellData and MutationType in reusable types module

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type MutationType = "fast" | "immortal" | "double_life" | null;
+
+export interface CellData {
+  hasBacteria: boolean;
+  mutationType: MutationType;
+  birthTime: number;
+  lifespan: number;
+}
+
+export interface CellProps {
+  hasBacteria: boolean;
+  mutationType: MutationType;
+}
